5567247a8f255edcfaa93e925ea388fcccc6583a
INFOR\robertosi <r.simonini@esaedro.com>
2023-08-22 12:11:14 +0200

    changed Constructuror and changed instance

--- Classes.js
+++ Classes.js
@@ -1,39 +1,30 @@
 class Pc {
-    constructor (name, brand, price) {
-        this.name = name;
-        this.brand = brand;
-        this.price = price;
-    }
+  constructor(name, brand, price) {
+    this.name = name;
+    this.brand = brand;
+    this.price = price;
+  }
 
-    print() {
-        console.log(this.name)
-        console.log(this.brand)
-        console.log(this.price + ' €');
-    }
+  print() {
+    console.log(this.name);
+    console.log(this.brand);
+    console.log(this.price + " €");
+  }
 }
 
 class NoteBook extends Pc {
-    constructor(noteBookSpecifics) {
-        super (
-            noteBookSpecifics.name,
-            noteBookSpecifics.brand,
-            noteBookSpecifics.price
-        )
-        this.ageOfRelease = noteBookSpecifics.ageOfRelease,
-        this.ram = noteBookSpecifics.ram;
-    }
+  constructor(name, brand, price, ageOfRelease, ram) {
+    super(name, brand, price);
+    (this.ageOfRelease = ageOfRelease), (this.ram = ram);
+  }
 
-    print () {
-        super.print();
-        console.log(`Questo notebook è stato rilasciato nel ${this.ageOfRelease} ed ha ${this.ram} gb di ram`)
-    }
+  print() {
+    super.print();
+    console.log(
+      `Questo notebook è stato rilasciato nel ${this.ageOfRelease} ed ha ${this.ram} gb di ram`
+    );
+  }
 }
 
-const noteBook = new NoteBook({
-    name: 'NoteBook Pro',
-    brand: 'Lenovo',
-    price: 2000,
-    ageOfRelease: 2016,
-    ram: 32
-});
+const noteBook = new NoteBook("NoteBook Pro", "Lenovo", 2000, 2016, 32);
 noteBook.print();
