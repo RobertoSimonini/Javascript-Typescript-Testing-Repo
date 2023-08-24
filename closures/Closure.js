d4a414905b58f82a34e6431d5ec0d809c4127dde
INFOR\robertosi <r.simonini@esaedro.com>
2023-08-24 17:37:20 +0200

    closure in another closure

--- closures/Closure.js
+++ closures/Closure.js
@@ -1,11 +1,14 @@
 function greetings(msg) {
-  return function who(name, sentence) {
-    console.log(`${msg}, ${name}, ${sentence}`);
-  };
+  function who(name, sentence) {
+    return function test() {
+      console.log(`${msg}, ${name}, ${sentence}`);
+    };
+  }
+  return who("Carlo", "Come stai?");
 }
 
 const hello = greetings("Hello");
 const pippo = greetings("Pippo");
 const howAreYou = greetings("Are you feeling good?");
 
-hello("Ciao", "Carlo");
+hello();
