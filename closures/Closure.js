247d046a59f1b4d5f533f72d30ca70f292a9ef0c
INFOR\robertosi <r.simonini@esaedro.com>
2023-08-25 10:01:57 +0200

    increase function closures

--- closures/Closure.js
+++ closures/Closure.js
@@ -1,20 +1,29 @@
-// function greetings(msg) {
-//   function who(name, sentence) {
-//     return function test() {
-//       console.log(`${msg}, ${name}, ${sentence}`);
-//     };
-//   }
-//   return who("Carlo", "Come stai?");
-// }
-
-const greetings = (msg) => {
-  return (name) => {
-    console.log(`${msg}, ${name}`);
-  };
-};
+function greetings(msg) {
+  function who(name, sentence) {
+    return function test() {
+      console.log(`${msg}, ${name}, ${sentence}`);
+    };
+  }
+  return who("Carlo", "Come stai?");
+}
 
 const hello = greetings("Hello");
 const pippo = greetings("Pippo");
 const howAreYou = greetings("Are you feeling good?");
 
-hello("Carlo");
+function count(step = 1) {
+  let count = 0;
+  return function increaseCount() {
+    count += step;
+    return console.log(count);
+  };
+}
+
+const increaseByOne = count(1);
+const increaseByThree = count(3);
+
+increaseByOne();
+increaseByOne();
+
+increaseByThree();
+increaseByThree();
